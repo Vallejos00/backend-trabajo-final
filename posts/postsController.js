48802fb58e5ac5cd32cac61f6d8d6813fa41a3d1
Vallejos00 <santivallejos.01@gmail.com>
2023-02-22 15:12:13 -0300

    union user y post

--- posts/postsController.js
+++ posts/postsController.js
@@ -1,27 +1,55 @@
+import { json } from "express";
 import token from "../utils/handleJWT.js";
+
 import Post from "./postsModel.js";
 
 
-const createNewPost = (req, res, next) => {
+const createNewPost =  (req, res, next) => {
     const newPost = new Post({ ...req.body });
-    console.log(req);
-    newPost.save((error) => {
+    
+    newPost.save ( async (error) => {
       if (error) return next(error);
-      res.status(200).json({ message: "New post saved"});
+      const resultado = await Post.aggregate([
+        {
+        $lookup:{
+          from: "users",
+          localField: "author",
+          foreignField: "_id",
+          as: "authorUser"
+        }
+      },
+      {$unwind:"$authorUser"},  
+      
+    ])
+res.status(200).json({ message: "New post saved"});
     }); 
 }
 
 
 
-const listAllPosts = (req, res, next) => {
-    Post.find()
-    .then((data) => {
-      !data.length ? next() : res.status(200).json(data);
-    })
-    .catch((error) => {
-      error.status = 500;
-      next(error);
-    });
+const listAllPosts = async (req, res, next) => {
+    const data = await Post.find()
+    if (!data.length) {
+     next()
+    } else{
+      const postWithUser = await Post.aggregate([
+        {
+        $lookup:{
+          from: "users",
+          localField: "author",
+          foreignField: "_id",
+          as: "user"
+        }
+      },
+      {$unwind:"$user"},
+      
+    ],  
+    )
+      
+      res.status(200).json({postWithUser})
+      
+    }
+
 }
 
 
--- posts/postsModel.js
+++ posts/postsModel.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
-const Schema = mongoose.Schema
+const {Schema, model} = mongoose
 
 const PostSchema = Schema({
     title: { type: String, required: true },
     body: { type: String, required: true },
+    author: {type: mongoose.Types.ObjectId, required: true},
     date: { type: Date, default: Date.now },
+    algo: { type: String}
 },
-{ timestamps: true }
+{ timestamps: true },
 )
+const Post = model("Post", PostSchema);
 
-const Post = mongoose.model("Post", PostSchema);
+
+
+
+
 export default Post
